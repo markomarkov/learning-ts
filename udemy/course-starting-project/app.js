a82134a2d87494076b2216a4a24c3b5b8d6e095e
Marko Markov <stormshadow973@gmail.com>
2024-04-30 14:58:33 +0300

    Refactor app.js and app.ts files to add a function for adding two numbers and printing the result

--- udemy/course-starting-project/app.js
+++ udemy/course-starting-project/app.js
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    return result;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var returnResult = add(number1, number2, printResult, resultPhrase);
+console.log("const returnResult: ".concat(returnResult));
