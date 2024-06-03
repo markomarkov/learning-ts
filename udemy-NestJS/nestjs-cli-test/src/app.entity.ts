63d28421d1ab52fe12fc2953f913aa6fbe7a3e59
Marko Markov <stormshadow973@gmail.com>
2024-06-03 12:09:58 +0300

    chore: Update app.entity.ts and package.json

--- udemy-NestJS/nestjs-cli-test/src/app.entity.ts
+++ udemy-NestJS/nestjs-cli-test/src/app.entity.ts
@@ -2,9 +2,9 @@ import { Entity } from 'typeorm';
 
 @Entity()
 export class AppEntity {
-  id: number;
-  name: string;
-  description: string;
-  createdAt: Date;
-  updatedAt: Date;
+    id: number;
+    name: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
