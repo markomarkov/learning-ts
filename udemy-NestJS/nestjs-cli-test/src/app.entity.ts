ce2a27990bbad734f208c25c1f60265452151a9e
Marko Markov <stormshadow973@gmail.com>
2024-06-03 13:53:30 +0300

    feat: Update app.entity.ts to use UUID as primary key

--- udemy-NestJS/nestjs-cli-test/src/app.entity.ts
+++ udemy-NestJS/nestjs-cli-test/src/app.entity.ts
@@ -1,10 +1,16 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class AppEntity {
-    id: number;
+    @PrimaryGeneratedColumn('uuid') 
+    // is a decorator that tells TypeORM to generate a primary key column in the database.
+    id: string;
+    @Column()
     name: string;
+    @Column()
     description: string;
+    @Column()
     createdAt: Date;
+    @Column()
     updatedAt: Date;
 }
